a509a19ab13dff6adee5feee5ea280a597c95afb
David M. Gross <dave@eorbit.net>
2025-03-24 16:21:33 -0700

    convert .md to .mdx, add components

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
             './src/styles/custom.css'
          ],
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/DavidMGross/TaxStrikeTactics',
 			},
 			sidebar: [
 				{
